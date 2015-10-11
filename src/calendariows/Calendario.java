27e55a2382cf5b5e9a39e84f5d79ffc6cd11faf8
roberto <roberto@dark>
2015-10-11 14:04:59 -0400

    Uso de mapas en la solucion.

--- src/calendariows/Calendario.java
+++ src/calendariows/Calendario.java
@@ -1,7 +1,9 @@
 package calendariows;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -11,32 +13,51 @@ class Calendario {
 
     private final int[] horasDeInicioValidos = {9, 10, 11, 14, 15, 16};
     private final int[] horasDeFinValidos = {10, 11, 12, 15, 16, 17};
-    private List<Horario> horarios;
+    private final Map<String, List<Horario>> reservas;
 
     public Calendario() {
-        horarios = new ArrayList<>();
+        reservas = new HashMap<>();
     }
 
     public boolean asignarHorario(Horario horario, Materia materia) {
         if (horario.estaEnRango(horasDeInicioValidos, horasDeFinValidos)) {
             if (!estaRegistrado(horario)) {
-                return horarios.add(horario);
+                List<Horario> horarios = reservas.get(materia.miCodigo());
+                if (horarios == null) {
+                    horarios = new ArrayList<>();    
+                }
+                
+                horarios.add(horario);
+                reservas.put(materia.miCodigo(), horarios);
+                return true;
             }
         }
         return false;
     }
 
     public int contarMaterias() {
-        return 1;
+        return reservas.size();
     }
 
     private boolean estaRegistrado(Horario horario) {
-        return horarios.contains(horario);
+        for (Horario horarioReservado : listarHorarios()) {
+            if (horarioReservado.equals(horario)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public int contarHorarios() {
-        throw new UnsupportedOperationException("SOFTURE: deberia devolver la "
-                + "cantidad de horarios que se registraron");
+        return listarHorarios().size();
+    }
+    
+    private List<Horario> listarHorarios () {
+        List<Horario> horarios = new ArrayList<>();
+        for(List lista : reservas.values()) {
+            horarios.addAll(lista);
+        }
+        return horarios;
     }
 
 }
--- src/calendariows/Materia.java
+++ src/calendariows/Materia.java
@@ -5,8 +5,14 @@ package calendariows;
  * @author sofunA
  */
 class Materia {
-
+    
+    private String codigo;
+    
     Materia(String codigo, String nombre, int duracion) {
+        this.codigo = codigo;
     }
     
+    public String miCodigo() {
+        return codigo;
+    }
 }
